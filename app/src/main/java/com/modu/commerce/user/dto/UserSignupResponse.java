b54cb6e62f7681829c4b76a2f08faf03c7d29d9d
wsj_6872 <wsj_6872@squarenet.co.kr>
2025-08-05 11:33:45 +0900

    gitignore 적용 안되는 현상 발생 > git 캐시 제거

--- app/src/main/java/com/modu/commerce/user/dto/UserSignupResponse.java
+++
@@ -1,9 +0,0 @@
-package com.modu.commerce.user.dto;
-
-import lombok.Data;
-
-@Data
-public class UserSignupResponse {
-    private String email;
-    private String nickname;
-}
--- app/src/main/java/com/modu/commerce/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.modu.commerce.user.service;
-
-import com.modu.commerce.user.dto.UserSignupRequest;
-import com.modu.commerce.user.dto.UserSignupResponse;
-
-public interface UserService {
-
-    public UserSignupResponse signup(UserSignupRequest request);
-}
