81fcca2685559ca06aba4a3e1ec36862607b32a0
tjdwns6872 <tjdwns6872@naver.com>
2025-08-11 14:40:02 +0900

    feat: 스웨거 JWT 토큰 추가

---
+++ app/src/main/java/com/modu/commerce/common/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.modu.commerce.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public OpenAPI openApi(){
+        SecurityScheme securityScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
+        return new OpenAPI().info(null).addSecurityItem(securityRequirement).schemaRequirement("BearerAuth", securityScheme);
+    }
+}
