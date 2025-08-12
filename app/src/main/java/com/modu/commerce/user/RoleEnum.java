26b1e267f87bc57e56d96337959ed5bc58d17a31
tjdwns6872 <tjdwns6872@naver.com>
2025-08-12 16:32:09 +0900

    feat: 회원 등급 Enum 이름 추가

--- app/src/main/java/com/modu/commerce/user/RoleEnum.java
+++ app/src/main/java/com/modu/commerce/user/RoleEnum.java
@@ -1,5 +1,15 @@
 package com.modu.commerce.user;
 
 public enum RoleEnum {
-    USER, ADMIN
+    USER("USER"), ADMIN("ADMIN");
+
+    private final String name;
+
+    RoleEnum(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
