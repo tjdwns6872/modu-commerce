3478f7fc86064ddeb079c95e24e302c553ffebab
tjdwns6872 <tjdwns6872@naver.com>
2025-09-16 19:13:46 +0900

    feat: 카테고리 쿼리 findByIdAndDeletedAtIsNull 추가

--- app/src/main/java/com/modu/commerce/category/infra/repository/CategoryRepository.java
+++ app/src/main/java/com/modu/commerce/category/infra/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.modu.commerce.category.infra.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.modu.commerce.category.domain.entity.ModuCategory;
@@ -11,6 +13,7 @@ public interface CategoryRepository extends JpaRepository<ModuCategory, Long>{
     boolean existsByParent_IsNullAndName(String name);
     boolean existsByParent_Id(Long parentId);
     boolean existsByIdAndDeletedAtIsNull(Long id);
+    Optional<ModuCategory> findByIdAndDeletedAtIsNull(Long id);
     Long countByParent_Id(Long parentId);
     Long countByParent_IdAndDeletedAtIsNull(Long parentId);
 }
